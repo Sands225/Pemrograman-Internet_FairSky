122366f6cbb28f098c0380b400c1ac80af55c6ea
Nomad37301 <agungpurnama100@gmail.com>
2025-12-09 20:54:07 +0800

    add file seeder booking

---
+++ database/seeders/BookingSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+
+class BookingSeeder extends Seeder
+{
+    public function run()
+    {
+        // Ambil ID User acak (selain admin)
+        $userIds = DB::table('users')->where('role', '!=', 'admin')->pluck('id')->toArray();
+
+        // Jika tidak ada user lain, pakai ID 1 (admin) sebagai fallback
+        if (empty($userIds)) $userIds = [1];
+
+        for ($i = 0; $i < 10; $i++) {
+            DB::table('bookings')->insert([
+                'user_id' => $userIds[array_rand($userIds)],
+                'booking_code' => strtoupper(Str::random(6)), // Generate kode unik 6 digit
+                'booking_date' => Carbon::now(),
+                'total_price' => 0, // Nanti diupdate otomatis oleh TicketSeeder
+                'payment_status' => 'Paid',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
+    }
+}
