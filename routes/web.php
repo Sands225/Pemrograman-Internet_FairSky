58cd4edbf281bda9e2d553e32c333af48734fdee
Nomad37301 <agungpurnama100@gmail.com>
2025-12-12 12:18:20 +0800

    add route kekny

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\FlightController;
 
 Route::get('/', [PageController::class, 'home'])->name('home');
 Route::get('/login', [PageController::class, 'login'])->name('login');
+Route::get('/flights', [FlightController::class, 'index'])->name('flights.index');
