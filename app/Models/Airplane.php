7f5d81988f9b92056f5a3be0873d20dedf960e50
Sands225 <sandikakadek2018@gmail.com>
2026-01-01 18:50:50 +0800

    updaeted airplane model

--- app/Models/Airplane.php
+++ app/Models/Airplane.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Airplane extends Model
 {
-    //
+    public function airplanes()
+    {
+        return $this->hasMany(Airplane::class);
+    }
+
+    public function airline()
+    {
+        return $this->belongsTo(Airline::class);
+    }
 }
